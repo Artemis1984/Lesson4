fbcb1b241f42ad6a99fcef0877bd325523e97aef
Artemis1984 <artak-minasyan-84@mail.ru>
2018-10-09 16:49:44 +0300

    Задание 4

--- src/Employee.java
+++ src/Employee.java
@@ -1,27 +1,27 @@
 public class Employee {
 
+    private String name;
+    private String position;
+    private String email;
+    private String phonenumber;
+    private int age;
+    private int salary;
 
-        private String name;
-        private String position;
-        private String email;
-        private String phonenumber;
-        private int age;
-        private int salary;
+    public Employee(String _name, String _position, String _email, String _phonenumber, int _age, int _salary){
+        this.name = _name;
+        this.position = _position;
+        this.email = _email;
+        this.phonenumber = _phonenumber;
+        this.age = _age;
+        this.salary = _salary;
+    }
 
-        public Employee(String _name, String _position, String _email, String _phonenumber, int _age, int _salary){
-            this.name = _name;
-            this.position = _position;
-            this.email = _email;
-            this.phonenumber = _phonenumber;
-            this.age = _age;
-            this.salary = _salary;
-        }
+    public void Show(){
+        System.out.print("ФИО - " + name + ", Должность - " + position + ", email - "
+                + email + ", Телефон - " + phonenumber + ", Возраст - " + age + ", Зарплата - " + salary + "$");
+    }
 
-        public void Show(){
-            System.out.print("ФИО - " + name + ", Должность - " + position + ", email - "
-                    + email + ", Телефон - " + phonenumber + ", Возраст - " + age + ", Зарплата - " + salary + "$");
-        }
-
-
-
+    public int GetAge(){
+        return age;
+    }
 }
--- src/EmployeeTest.java
+++ src/EmployeeTest.java
@@ -1,9 +1,27 @@
 public class EmployeeTest {
 
     public static void main(String[] args) {
-        Employee employee = new Employee("Иван Иванович Иванов", "Младший разработчик", "Ivanov@mail.ru",
-                "89999999999", 32, 2000);
-        employee.Show();
-    }
 
+
+        Employee[] employees = new Employee[5];
+
+        employees[0] = new Employee("Иван Иванович Иванов", "Младший разработчик", "Ivanov@mail.ru",
+                "89999999999", 32, 2000);
+        employees[1] = new Employee("Федор Федорович Федоров", "Младший разработчик", "Fedorov@mail.ru",
+                "84951231234", 35, 2000);
+        employees[2] = new Employee("Сергей Сергеевич Сергеев", "Младший разработчик", "Ivanov@mail.ru",
+                "84956564312", 38, 2000);
+        employees[3] = new Employee("Кирилл Кириллович Кириллов", "Младший разработчик", "Kirillov@mail.ru",
+                "89999999999", 41, 2000);
+        employees[4] = new Employee("Антон Антонович Антонов", "Младший разработчик", "Antonov@mail.ru",
+                "89999999999", 45, 2000);
+
+
+        for (Employee oldEmployee : employees){
+            System.out.println();
+            if (oldEmployee.GetAge() > 40){
+                oldEmployee.Show();
+            }
+        }
+    }
 }
