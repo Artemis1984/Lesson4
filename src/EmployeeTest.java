32db76ba0ada02f1a11e404deaa0a532c02af692
Artemis1984 <artak-minasyan-84@mail.ru>
2018-10-09 14:04:04 +0300

    Задание 1, 2, 3

---
+++ src/EmployeeTest.java
@@ -0,0 +1,9 @@
+public class EmployeeTest {
+
+    public static void main(String[] args) {
+        Employee employee = new Employee("Иван Иванович Иванов", "Младший разработчик", "Ivanov@mail.ru",
+                "89999999999", 32, 2000);
+        employee.Show();
+    }
+
+}
